2ec52921fd63ac985f019a25ef74e838f0901342
Adi023 <adityapandhare050@gmail.com>
2024-06-11 23:13:53 +0530

    This is task time tracker project as Backend

---
+++ src/test/java/com/ttprojects/office/TtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttprojects.office;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
